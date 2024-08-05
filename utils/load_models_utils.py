c204e00896050692f010df9b4dd8e54c10a872ef
smthemex <138738845+smthemex@users.noreply.github.com>
2024-08-05 21:58:10 +0800

    Update load_models_utils.py
    add

--- utils/load_models_utils.py
+++ utils/load_models_utils.py
@@ -1,13 +1,11 @@
 import yaml
 import torch
-from diffusers import StableDiffusionXLPipeline, DiffusionPipeline, AutoPipelineForText2Image, StableDiffusionPipeline
+from diffusers import StableDiffusionXLPipeline
 from .pipeline import PhotoMakerStableDiffusionXLPipeline
-#from .pipeline_v2 import PhotoMakerStableDiffusionXLPipeline as PhotoMakerStableDiffusionXLPipelineV2
+
 import os
 import sys
-import diffusers
-dif_version = str(diffusers.__version__)
-dif_version_int= int(dif_version.split(".")[1])
+
 dir_path = os.path.dirname(os.path.abspath(__file__))
 path_dir = os.path.dirname(dir_path)
 file_path = os.path.dirname(path_dir)
@@ -39,19 +37,20 @@ def get_lora_dict():
 datas = get_lora_dict()
 lora_lightning_list = datas["lightning_xl_lora"]
 
-
-def load_models(path,model_type,single_files,use_safetensors,photomaker_path,lora,lora_path,trigger_words,lora_scale):
-    photoname=os.path.split(photomaker_path)[-1]
+def load_models(path,model_type,single_files,use_safetensors,photomake_mode,photomaker_path,lora,lora_path,trigger_words,lora_scale):
     if model_type == "txt2img":
         if single_files:
-            if dif_version_int>=28:
+            try:
                 pipe = StableDiffusionXLPipeline.from_single_file(
-                    pretrained_model_link_or_path=path, original_config=original_config_file, torch_dtype=torch.float16)
-
-            else:
-                pipe = StableDiffusionXLPipeline.from_single_file(
-                    pretrained_model_link_or_path=path, original_config_file=original_config_file, torch_dtype=torch.float16
-                )
+                    path, original_config=original_config_file, torch_dtype=torch.float16)
+            except:
+                try:
+                    pipe = StableDiffusionXLPipeline.from_single_file(
+                        path, original_config_file=original_config_file,
+                        torch_dtype=torch.float16
+                    )
+                except:
+                    raise "load pipe error"
 
         else:
             pipe = StableDiffusionXLPipeline.from_pretrained(
@@ -67,52 +66,61 @@ def load_models(path,model_type,single_files,use_safetensors,photomaker_path,lor
                 pipe.fuse_lora(adapter_names=[trigger_words, ], lora_scale=lora_scale)
 
     elif model_type == "img2img":
-
-        if single_files:
-            # print("loading from a single_files")
-            if dif_version_int >= 28:
-                pipe = PhotoMakerStableDiffusionXLPipeline.from_single_file(
-                    pretrained_model_link_or_path=path, original_config=original_config_file,
-                    torch_dtype=torch.float16, use_safetensors=use_safetensors)
+        if photomake_mode=="v1":
+            if single_files:
+                # print("loading from a single_files")
+                try:
+                    pipe = PhotoMakerStableDiffusionXLPipeline.from_single_file(
+                        path, original_config=original_config_file,
+                        torch_dtype=torch.float16, use_safetensors=use_safetensors)
+                except:
+                    try:
+                        pipe = PhotoMakerStableDiffusionXLPipeline.from_single_file(
+                            path, original_config_file=original_config_file,
+                            torch_dtype=torch.float16, use_safetensors=use_safetensors
+                        )
+                    except:
+                        raise "load pipe error"
+            
             else:
-                pipe = PhotoMakerStableDiffusionXLPipeline.from_single_file(
-                    pretrained_model_link_or_path=path, original_config_file=original_config_file,
-                    torch_dtype=torch.float16, use_safetensors=use_safetensors
+                pipe = PhotoMakerStableDiffusionXLPipeline.from_pretrained(
+                    path, torch_dtype=torch.float16, use_safetensors=use_safetensors
                 )
-        else:
-            pipe = PhotoMakerStableDiffusionXLPipeline.from_pretrained(
-                path, torch_dtype=torch.float16, use_safetensors=use_safetensors
+            pipe.load_photomaker_adapter(
+                photomaker_path,
+                subfolder="",
+                weight_name="photomaker-v1.bin",
+                trigger_word="img"  # define the trigger word
             )
-        pipe.load_photomaker_adapter(
-            photomaker_path,
-            subfolder="",
-            weight_name=photoname,
-            trigger_word="img"  # define the trigger word
-        )
-        # else:
-        #     if single_files:
-        #         # print("loading from a single_files")
-        #         if dif_version_int >= 28:
-        #             pipe = PhotoMakerStableDiffusionXLPipelineV2.from_single_file(
-        #                 pretrained_model_link_or_path=path, original_config=original_config_file,
-        #                 torch_dtype=torch.float16, use_safetensors=use_safetensors)
-        #         else:
-        #             pipe = PhotoMakerStableDiffusionXLPipelineV2.from_single_file(
-        #                 pretrained_model_link_or_path=path, original_config_file=original_config_file,
-        #                 torch_dtype=torch.float16, use_safetensors=use_safetensors
-        #             )
-        #     else:
-        #         pipe = PhotoMakerStableDiffusionXLPipelineV2.from_pretrained(
-        #             path, torch_dtype=torch.float16, use_safetensors=use_safetensors
-        #         )
-        #     # define the trigger word
-        #     pipe.load_photomaker_adapter(
-        #         photomaker_path,
-        #         subfolder="",
-        #         weight_name=photoname,
-        #         trigger_word="img",  
-        #         pm_version= 'v2',
-        #     )
+        else:
+            from .pipeline_v2 import PhotoMakerStableDiffusionXLPipeline as PhotoMakerStableDiffusionXLPipelineV2
+            if single_files:
+                # print("loading from a single_files")
+                try:
+                    pipe = PhotoMakerStableDiffusionXLPipelineV2.from_single_file(
+                        path, original_config=original_config_file,
+                        torch_dtype=torch.float16, use_safetensors=use_safetensors)
+                except:
+                    try:
+                        pipe = PhotoMakerStableDiffusionXLPipelineV2.from_single_file(
+                            path, original_config_file=original_config_file,
+                            torch_dtype=torch.float16, use_safetensors=use_safetensors
+                        )
+                    except:
+                        raise "load pipe error"
+            else:
+                pipe = PhotoMakerStableDiffusionXLPipelineV2.from_pretrained(
+                    path, torch_dtype=torch.float16, use_safetensors=use_safetensors
+                )
+            # define the trigger word
+            pipe.load_photomaker_adapter(
+                photomaker_path,
+                subfolder="",
+                weight_name="photomaker-v2.bin",
+                trigger_word="img",
+                pm_version= 'v2',
+            )
+            
         if lora != "none":
             if lora in lora_lightning_list:
                 pipe.load_lora_weights(lora_path)
@@ -120,7 +128,6 @@ def load_models(path,model_type,single_files,use_safetensors,photomaker_path,lor
             else:
                 pipe.load_lora_weights(lora_path, adapter_name=trigger_words)
                 pipe.fuse_lora(adapter_names=[trigger_words, ], lora_scale=lora_scale)
-
 
     else:
         raise f"using{model_type}node,must choice{model_type}type in model_loader node"
